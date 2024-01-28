3446ab314c16adc524f852db0fbdb8f36c7bd427
Recep Karabacak <123902602+Reckie-G@users.noreply.github.com>
2024-01-28 23:01:32 +0300

    Yo guys, this is my first commit

---
+++ NumberGuessingGame/NumberGuessingGame/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int TutulanSayi = rnd.Next(0, 1000);
+        int Sayi = 0, TahminHakki = 10;
+
+        while (TahminHakki > 0)
+        {
+            Console.WriteLine("1-1000 arasında sayı giriniz:");
+            Sayi = Convert.ToInt32(Console.ReadLine());
+            TahminHakki--;
+
+            if (TutulanSayi == Sayi)
+            {
+                Console.WriteLine("Sayıyı {0}. hakkında doğru tahmin ettiniz.", TahminHakki);
+                break;
+            }
+            else
+                if (TutulanSayi > Sayi)
+            {
+                Console.WriteLine("Tutulan sayı daha büyük");
+            }
+            else if (TutulanSayi < Sayi)
+            {
+                Console.WriteLine("Tutulan sayı daha küçük");
+            }
+            Console.WriteLine("Kalan tahmin hakkınız {0}", TahminHakki);
+        }
+        if (TahminHakki == 0)
+        {
+            Console.WriteLine("Sayıyı tahmin edemediniz. Tutulan sayı = {0}", TutulanSayi);
+        }
+        Console.ReadLine();
+    }
+}
---
+++ NumberGuessingGameV2/NumberGuessingGameWow/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("1 ile 1000 arasında bir sayı seçin ve ben tahmin etmeye çalışacağım.");
+        Console.Write("Tutulan sayıyı girin: ");
+
+        if (int.TryParse(Console.ReadLine(), out int tutulanSayi) && tutulanSayi >= 1 && tutulanSayi <= 1000)
+        {
+            int[] kuvvetler = { 512,256,128,64, 32, 16, 8, 4, 2, 1 };
+            int tahmin = 0;
+            int denemeSayisi = 0;
+            string cevap;
+
+            Console.WriteLine("Tahminlerimi kontrol edin tuttuğunuz sayı daha yüksekse 'y', düşükse 'd', doğruysa 'e' tuşlayın.");
+
+            do
+            {
+                tahmin += kuvvetler[denemeSayisi];
+
+                Console.WriteLine($"Tahminim: {tahmin}");
+                Console.Write("Cevap (d/y/e): ");
+                cevap = Console.ReadLine().ToLower();
+
+                if (cevap == "d")
+                {
+                    tahmin -= kuvvetler[denemeSayisi];
+                }
+                else if (cevap == "y")
+                {
+                    tutulanSayi -= kuvvetler[denemeSayisi];
+                }
+
+                denemeSayisi++;
+
+            } while (cevap != "e" && denemeSayisi < 10);
+
+            if (cevap == "e")
+            {
+                Console.WriteLine($"{denemeSayisi}. denemede doğru tahmin ettim.");
+            }
+            else
+            {
+                Console.WriteLine($"Üzgünüm. 10 denemede doğru tahmin edemedim. Tutulan sayı: {tutulanSayi}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Geçerli bir sayı girmediniz veya 1 ile 1000 arasında bir sayı girmediniz.");
+        }
+    }
+}
